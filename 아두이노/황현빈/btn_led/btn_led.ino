65696bcd3197b5f938410be9a0b47b74b31756bf
BingSsu <caroink@naver.com>
2019-06-02 23:01:17 +0900

    btn_led

---
+++ 아두이노/황현빈/btn_led/btn_led.ino
@@ -0,0 +1,12 @@
+void setup() {
+ pinMode(4,INPUT);
+ pinMode(2, OUTPUT); 
+}
+void loop() {
+  if (digitalRead(4)==LOW){
+    digitalWrite(2, HIGH);
+  }
+  else{
+    digitalWrite(2, LOW);
+  }
+}
